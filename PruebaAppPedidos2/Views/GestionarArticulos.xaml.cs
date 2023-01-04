b195a87ca270601004268e3598484dc2d6282729
JorgeRicardo <jorge.ricardo@utp.edu.co>
2023-01-04 16:07:29 -0500

    Se finalizo la pantalla de GestionArticulos

--- PruebaAppPedidos2/Views/GestionarArticulos.xaml.cs
+++ PruebaAppPedidos2/Views/GestionarArticulos.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PruebaAppPedidos2.ViewsModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,7 @@ namespace PruebaAppPedidos2.Views
 		public GestionarArticulos ()
 		{
 			InitializeComponent ();
+			BindingContext= new ViewModelGestionarArticulos(Navigation);
 		}
 	}
 }
---
+++ PruebaAppPedidos2/ViewsModels/ViewModelGestionarArticulos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PruebaAppPedidos2.Views;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PruebaAppPedidos2.ViewsModels
+{
+    class ViewModelGestionarArticulos : BaseViewModel
+    {
+        //Variables
+
+
+        //Constructor
+        public ViewModelGestionarArticulos(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        //Objetos
+
+
+
+        //Procesos
+        public async Task irAVerGrupos()
+        {
+            await Navigation.PushAsync(new VerGrupos());
+        }
+
+        //Comandos
+        public ICommand irAVerGruposcommand => new Command(async () => await irAVerGrupos());
+    }
+}
